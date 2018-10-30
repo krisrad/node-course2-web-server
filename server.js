c538732c2da034c43a11e44ad600e2418ae9d1af
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2018-10-30 14:01:00 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// app.use((req, res, next) => {
+//     res.render("maintenance.hbs");
+// });
+app.use(express.static(__dirname+"/public"));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', ()=> new Date().getFullYear());
+hbs.registerHelper('screamIt', (text)=>text.toUpperCase());
+
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        pageTitle: "Home Page",
+        welcomeMessage: "Welcome to the wonderful world of node and express"
+    })    
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About Page"
+    });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "Unable to fulfill this request"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
